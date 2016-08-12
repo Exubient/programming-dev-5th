1dcfa3226ad68fe62f36b7c1bd4e470da1c6c77d
Hyun Joong Kim <henrynicholas09@gmail.com>
2016-08-12 16:33:38 +0900

    new setting

---
+++ askdjango/settings/prod.py
@@ -0,0 +1,16 @@
+from .common import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+	'default' : {
+		'ENGINE':'django.db.backends.postgresql'.
+		'NAME' : 'ubuntu',
+		'USER' : 'ubuntu',
+		'PASSWORD' : 'withaskdjango!',
+		'HOST' : '127.0.0.1',
+	},
+	}
+
